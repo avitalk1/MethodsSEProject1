727bcff561dbb24ddbf82eead3fc39074991714f
linoy-noah <linoy.noah1@gmail.com>
2020-07-30 21:27:01 +0300

    commit basic main

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -1,64 +1,21 @@
-// #include "HandleIO.h"
-// #include <windows.h>
-// #include <stdio.h>
-// #include <iostream>
-// #include <string>
-// using namespace std;
 
-// int main(int argc, char const *argv[])
-// {
-//     // Panel main_panel({0,0}, )
-//     // return 0;
-// }
-
-
-
-
-// #include "handleIO.h"
+#pragma once
 #include <windows.h>
 #include <stdio.h>
 #include <iostream>
 #include <string>
 #include "TextBox.h"
 #include "Component.h"
-// #include "checkList.h"
-// #include "textBox.h"
-// #include "Label.h"
+#include "Panel.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "try";
-    DWORD TC = FOREGROUND_RED;
-    DWORD BC = BACKGROUND_BLUE;
-    TextBox* t = new TextBox(3,3,{0,0},TC,BC,NONE);
-    // TextBox t2(3,3,{0,0},FOREGROUND_RED,BACKGROUND_BLUE,NONE);
-    // t2->_draw();
-    t->_draw();
-    cout << "END";
-    // TextBox* T= new TextBox(3,3,{0,0},FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY,BACKGROUND_BLUE,NONE);
-    // TextBox tb1({5, 15}, 13, 2);
-    // tb1.setColors(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY, BACKGROUND_BLUE);
-    // string s = "head";
-    // string list[5];
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     list[i] = "op" + to_string(i + 1);
-    // }
-    // checkBox l1(s, {1, 1}, list, 5);
-    // l1.setColors(FOREGROUND_RED, BACKGROUND_GREEN | BACKGROUND_RED);
-    // Label t("hello world", {5, 19});
-    // t.setColors(FOREGROUND_GREEN, BACKGROUND_BLUE | BACKGROUND_RED);
+    Panel* main_panel = new Panel({0,0}, BACKGROUND_BLUE, FOREGROUND_GREEN);
 
-    // HandleIO io;
-
-    // io.addComp(tb1);
-    // io.addComp(l1);
-    // io.addComp(t);
-
-    // io.IOstart();
-    /* code */
-
+    //to add components to the console do: main_panel.addComponent(...)
+    HandleIO* io = HandleIO::init(main_panel);
+    io->IOstart();
     return 0;
 }
